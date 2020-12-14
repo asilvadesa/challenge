c5dd85d634282f73cb1e7a5b1c18622d7a0d8d21
Anderson de Sa <asilvadesa@gmail.com>
2020-12-13 22:48:30 -0400

    Adiconando validacao para feature base de conhecimento da pagina de login

---
+++ functional-test/src/test/java/br/com/challenge/core/features/acessar_base_conhecimento.feature
@@ -0,0 +1,11 @@
+# language: pt
+Funcionalidade: Acessar Base de Conhecimento
+  Como usuario
+  Gostaria de acessar Base de Conhecimento
+
+
+  Cenario: Acessar base de conhecimento com sucesso
+    Dado que desejoa cessar a url "https://financeiro.hostgator.com.br/"
+    E clico em acessar agora
+    Então abre outra aba do navegador com o titulo "Suporte HostGator"
+
---
+++ functional-test/src/test/java/br/com/challenge/core/steps/BaseConhecimento.java
@@ -0,0 +1,41 @@
+package br.com.challenge.core.steps;
+
+import br.com.challenge.core.pageobjects.LoginPageObject;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseConhecimento {
+    private WebDriver driver;
+    private LoginPageObject loginPageObject;
+
+    @Dado("que desejoa cessar a url {string}")
+    public void queDesejoaCessarAUrl(String url) {
+        driver =  new ChromeDriver();
+        loginPageObject = new LoginPageObject(driver);
+        loginPageObject.open(url);
+    }
+
+    @Dado("clico em acessar agora")
+    public void clicoEmAcessarAgora() {
+        loginPageObject.cliqueBaseConhecimento();
+    }
+    @Então("abre outra aba do navegador com o titulo {string}")
+    public void abreOutraAbaDoNavegadorComOTitulo(String tituloEsperado) {
+        List<String> abas = new ArrayList<>(driver.getWindowHandles());
+        String tituloBrowser = driver.switchTo().window(abas.get(1)).getTitle();
+        try {
+            Assert.assertEquals(tituloEsperado, tituloBrowser );
+        }finally {
+            driver.close();
+        }
+    }
+
+
+
+}
