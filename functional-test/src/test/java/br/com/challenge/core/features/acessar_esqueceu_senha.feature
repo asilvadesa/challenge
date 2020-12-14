af201b670218676330a78a06c90967c72a7664f6
Anderson de Sa <asilvadesa@gmail.com>
2020-12-13 21:36:51 -0400

    Adicionando feature esqueceu a senha da pagina de login

---
+++ functional-test/src/test/java/br/com/challenge/core/features/acessar_esqueceu_senha.feature
@@ -0,0 +1,11 @@
+# language: pt
+  Funcionalidade: Acessar Esqueceu a Senha
+    Como usuario
+    Gostaria de acessar a opção esqueceu a senha
+
+
+  Cenario: Acessar esqueceu a senha com sucesso
+    Dado que desejo acessar a apliacação na url "https://financeiro.hostgator.com.br/"
+    E clico em esqueceu a senha
+    Então sou redirecionado para pagina de esqueceu a senha que possui titulo "Redefinição de senha"
+
---
+++ functional-test/src/test/java/br/com/challenge/core/steps/EsqueceuSenhaSteps.java
@@ -0,0 +1,39 @@
+package br.com.challenge.core.steps;
+
+import br.com.challenge.core.pageobjects.LoginPageObject;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Então;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EsqueceuSenhaSteps {
+
+    private WebDriver driver;
+    private LoginPageObject loginPageObject;
+
+    @Dado("que desejo acessar a apliacação na url {string}")
+    public void queDesejoAcessarAApliacaçãoNaUrl(String url) {
+        driver = new ChromeDriver();
+        loginPageObject = new LoginPageObject(driver);
+        loginPageObject.open(url);
+    }
+
+    @Dado("clico em esqueceu a senha")
+    public void clicoEmEsqueceuASenha() {
+        loginPageObject.cliqueEsqueceuSenha();
+    }
+
+    @Então("sou redirecionado para pagina de esqueceu a senha que possui titulo {string}")
+    public void souRedirecionadoParaPaginaDeEsqueceuASenhaQuePossuiTitulo(String titulo) {
+        String logoTitulo = driver.findElement(By.className("logo-title")).getText();
+        try{
+            Assert.assertEquals(titulo, logoTitulo);
+        } finally {
+            driver.close();
+        }
+    }
+}
