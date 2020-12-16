fe5c8149caf2fac63b82984671479a3d9a1ab737
Anderson de Sa <asilvadesa@gmail.com>
2020-12-16 00:42:26 -0400

    Adionando casos de teste

--- rest-test/src/test/java/br/com/challenge/core/BuscaCEPTest.java
+++ rest-test/src/test/java/br/com/challenge/core/BuscaCEPTest.java
@@ -1,17 +1,15 @@
 package br.com.challenge.core;
 
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class BuscaCEPTest {
 
     @Test
-    public void naoDeve_RetornarEnderecoComCEPVazio(){
+    public void naoDeve_RetornarEnderecoCEPVazio(){
 
         given()
                 .contentType(ContentType.JSON)
@@ -25,44 +23,85 @@ public class BuscaCEPTest {
     }
 
     @Test
-    public void naoDeve_RetornarEnderecoComCEPInvalido(){
+    public void naoDeve_RetornarEnderecoCEPComApenasUmDigito(){
         given()
         .when()
                 .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/1")
         .then()
-            .log().all()
             .statusCode(400)
-
+            .body("notifications.field", hasItem("CEP"))
+            .body("notifications.level", hasItem("VALIDATION_ERROR"))
         ;
     }
 
+    @Test
+    public void naoDeve_RetornaEnderecoCEPComMaisDeOitoDigitosNumericos(){
+        given()
+        .when()
+                .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/690741005")
+        .then()
+                .statusCode(400)
+                .body("notifications.field", hasItem("CEP"))
+                .body("notifications.level", hasItem("VALIDATION_ERROR"));
+    }
 
-    public String buscaRegiaoCep(Integer num){
+    @Test
+    public void naoDeve_RetornarEnderecoCEPContendoDigitosAlfaNumericos(){
+        given()
+        .when()
+                .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/A69074100")
+        .then()
+                .body("data", nullValue());
+    }
 
-        switch (num){
-            case 0:
-            case 1:
-                return "SP";
-            case 2:
-                return "RJ, ES";
-            case 3:
-                return "MG";
-            case 4:
-                return "BA, SE";
-            case 5:
-                return "PB, AL, PB, RN";
-            case 6:
-                return "CE, PI, MA, PA, AM, AC, AP, RR";
-            case 7:
-                return "DF, GO, TO, MS, RO";
-            case 8:
-                return "PR, SC";
-            case 9:
-                return "RS";
-            default:
-                throw new IllegalArgumentException("CEP Invalido");
-        }
+    @Test
+    public void naoDeve_RetornarEnderecoCEPContendoDigitosCaracteresEspeciais(){
+        given()
+        .when()
+                .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/13165-000(*")
+        .then()
+                .body("data", nullValue());
+    }
 
+    @Test
+    public void deve_RetornarCodigoStatus404ComCEPValidoMasNaoExistente(){
+        given()
+        .when()
+                .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/69070109")
+        .then()
+                .statusCode(404)
+                .body("data", nullValue());
+    }
+
+    @Test
+    public void deve_RetornarMensagemNotAllowParaMetodoPostCEPValido(){
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .post("https://financeiro.hostgator.com.br/api/v3/checkout/cep/69074100")
+        .then()
+                .body("message", is("Method not allowed. Must be one of: OPTIONS, GET"));
+    }
+
+    @Test
+    public void deve_RetornarMensagemNotAllowParaMetodoPostCEPInvalido(){
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .post("https://financeiro.hostgator.com.br/api/v3/checkout/cep/74100")
+        .then()
+                .body("message", is("Method not allowed. Must be one of: OPTIONS, GET"));
+    }
+
+    @Test
+    public void deve_RetornarEnderecoParaCEPValidoExistente(){
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get("https://financeiro.hostgator.com.br/api/v3/checkout/cep/20751060")
+        .then()
+                .body("data", notNullValue())
+                .statusCode(200);
     }
 
 }
